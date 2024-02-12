428eeb62503eb4823402172a35b54bc3d048a92d
Evan Kysley <evan@kysley.com>
2024-02-11 20:05:13 -0500

    update color

--- app/components/weave-info.tsx
+++ app/components/weave-info.tsx
@@ -16,8 +16,8 @@ export function WeaveInfo() {
     <WeaveBranding>
       <Text className="text-center">
         <span className="font-bold italic">weave</span> is a{" "}
-        <span className="text-blue-500">zero knowledge</span> peer2peer file and
-        secret sharing service. <br />
+        <span className="text-emerald-500">zero knowledge</span> peer2peer file
+        and secret sharing service. <br />
         No files or secrets are handled by a middleman or server.
       </Text>
     </WeaveBranding>
